869bcef4fd2c81e47e612d896f963a7bdd7558b6
Hong Beomsun <ghsqjatns@naver.com>
2021-09-18 13:59:46 +0900

    Delete temp directory

--- temp/blo_web/blo_web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path(‘api/’, include(‘api.urls’)),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
-]
