85037b09cb0b3a2b901d58a266d41808d9e0e4ab
Moon Kyung Song <moonsong98@postech.ac.kr>
2021-10-02 09:10:48 +0000

    Create Header

--- WEB/frontend/src/App.tsx
+++ WEB/frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import FireArmsPage from "./pages/FireArmsPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/">
           <LoginPage />
