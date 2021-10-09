8dc29280305bf984b06407fc7ec4ad71d213090e
kojjok97 <76581333+kojjok97@users.noreply.github.com>
2021-10-09 00:28:25 +0000

    python 과 node.js 연결

--- fabric-samples/blomy/javascript/rest.js
+++ fabric-samples/blomy/javascript/rest.js
@@ -10,15 +10,17 @@ app.use(cors());
 
 const router = express.Router()
 
+// 의미없는 테스트 페이지 구현 웹페이지의 가장 초기화면 
 router.route('/').get((req,res)=>{
    res.send('<h1>test page</h1>')
 })
 
 
-router.route('/query').get((req,res)=>{
-    const param = req.query.serial
+//query 요청에 대한 응답 부분 총기에관한 serial번호를 입력받아서 AssetHistory(transaction)을 json형태로 출력하는 부분
+router.route('/query/:serial').get((req,res)=>{
+    const param = String(req.params.serial)
     console.log(param)
-    getHistory(param).then((value)=>{
+    getHistory(param).then((value)=>{ // 하이퍼레저에서 query에 대한 응답을 가져오는 부분
         res.writeHead(200, {"Content-Type":'text/html;charset=utf8'});
 		res.write(value)
 		res.end()
