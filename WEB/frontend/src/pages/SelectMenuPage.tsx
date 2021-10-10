4f59ce3520e94970a216bcd0d469085e53337f90
Moon Kyung Song <moonsong98@postech.ac.kr>
2021-10-10 08:19:33 +0000

    Change Style of Menu Selection Page

--- WEB/frontend/src/pages/SelectMenuPage.tsx
+++ WEB/frontend/src/pages/SelectMenuPage.tsx
@@ -1,23 +1,41 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { useHistory } from "react-router-dom";
+import Firearm from "../images/firearm.png";
+import FoodData from "../images/fooddata.png";
 
 export default function SelectMenuPage() {
   const history = useHistory();
   return (
     <div
       style={{
+        paddingTop: "10%",
+        paddingLeft: "10%",
+        paddingRight: "10%",
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "space-evenly",
       }}
     >
-      <Button>부식</Button>
+      <Button
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <img src={FoodData} alt="FoodData" />
+        <div style={{ color: "#B695E5", fontSize: "3em" }}>부식 작전</div>
+      </Button>
       <Button
         onClick={() => {
           history.push("/firearms");
         }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
       >
-        총기
+        <img src={Firearm} alt="FoodData" />
+        <div style={{ color: "#B695E5", fontSize: "3em" }}>총기 수불</div>
       </Button>
     </div>
   );
