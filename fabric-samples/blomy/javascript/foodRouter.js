4f3f7c9e9fc22ca8fc5cca4e8c18774f0d4c5eda
kojjok97 <76581333+kojjok97@users.noreply.github.com>
2021-10-11 13:03:29 +0000

    Food Rest 와 Firearm Rest 기능 추가

--- fabric-samples/blomy/javascript/foodRouter.js
+++ fabric-samples/blomy/javascript/foodRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.route('/queryUnit/:affiliatedUnit').get((req,res)=>{
     const affiliatedUnit = String(req.params.affiliatedUnit);
-    console.log('---------queryUnit호출---------');
+    console.log('---------부대 부식 정보 조회 호출---------');
     console.log(`affiliatedUnit = ${affiliatedUnit}`);
     foodChaincode.foodQuery(affiliatedUnit).then((value)=>{
         res.writeHead(200, {"Content-Type":'text/html;charset=utf8'});
@@ -15,63 +15,78 @@ router.route('/queryUnit/:affiliatedUnit').get((req,res)=>{
         res.end();
     });
 });
+
+router.route('/getUnitFoodHistory/:affiliatedUnit/:foodName').get((req,res)=>{
+    const affiliatedUnit = String(req.params.affiliatedUnit);
+    const foodName = String(req.params.foodName);
+    console.log('------------부대의 음식 트랜젝션 조회 호출------------');
+    console.log(`affiliatedUnit = ${affiliatedUnit}`);
+    console.log(`foodName = ${foodName}`);
+    foodChaincode.modifyFoodTransaction(['getUnitFoodHistory', affiliatedUnit, foodName]).then((value)=>{
+        res.writeHead(200,{"Content-Type":'text/html;charset=utf8'});
+        res.write(value);
+        res.end();
+    });
+
+})
+
 router.route('/createUnit/:affiliatedUnit').get((req,res)=>{
     const affiliatedUnit = String(req.params.affiliatedUnit);
     foodChaincode.modifyFoodTransaction(['createUnit',affiliatedUnit]).then((value)=>{
         res.writeHead(200,{"Content-Type":'text/html;charset=utf8'});
         res.write(value);
         res.end();
-    })
-})
+    });
+});
 router.route('/checkinFood/:affiliatedUnit/:foodName/:amount').get((req,res)=>{
     const affiliatedUnit = String(req.params.affiliatedUnit);
     const foodName = String(req.params.foodName);
     const amount = String(req.params.amount);
-    console.log('-------checkinFood 호출--------');
-    console.log(`affiliatedUnit = ${affiliatedUnit} `)
+    console.log('-------부식 반입 호출--------');
+    console.log(`affiliatedUnit = ${affiliatedUnit} `);
     console.log(`foodName = ${foodName}`);
     console.log(`amount = ${amount}`);
     foodChaincode.modifyFoodTransaction(['checkinFood',affiliatedUnit,foodName,amount]).then((value)=>{
         res.writeHead(200,{"Content-Type":'text/html;charset=utf8'});
         res.write(value);
         res.end();
-    })
-})
+    });
+});
 
 router.route('/checkoutFood/:affiliatedUnit/:foodName/:amount').get((req,res)=>{
     const affiliatedUnit = String(req.params.affiliatedUnit);
     const foodName = String(req.params.foodName);
     const amount = String(req.params.amount);
-    console.log('-------checkoutFood 호출--------');
-    console.log(`affiliatedUnit = ${affiliatedUnit} `)
+    console.log('-------부식 불출 호출--------');
+    console.log(`affiliatedUnit = ${affiliatedUnit} `);
     console.log(`foodName = ${foodName}`);
     console.log(`amount = ${amount}`);
     foodChaincode.modifyFoodTransaction(['checkoutFood',affiliatedUnit,foodName,amount]).then((value)=>{
         res.writeHead(200,{"Content-Type":'text/html;charset=utf8'});
         res.write(value);
         res.end();
-    })
-})
+    });
+});
 
 router.route('/GetUnitHistory/:affiliatedUnit').get((req,res)=>{
     const affiliatedUnit = String(req.params.affiliatedUnit);
-    console.log('--------GetUnitHistory 호출---------');
+    console.log('--------부대 거래 정보 조회 호출---------');
     console.log(`affiliatedUnit = ${affiliatedUnit}`)
     foodChaincode.modifyFoodTransaction(['GetUnitHistory',affiliatedUnit]).then((value)=>{
         res.writeHead(200,{"Content-Type":'text/html;charset=utf8'});
         res.write(value);
         res.end();
-    })
-})
+    });
+});
 
 router.route('/queryAllUnits').get((req,res)=>{
-    console.log('-------queryAllUnits-------');
+    console.log('-------모든 부대 거래 정보 조회 호출-------');
     foodChaincode.modifyFoodTransaction(['queryAllUnits']).then((value)=>{
         res.writeHead(200,{"Content-Type":'text/html;charset=utf8'});
         res.write(value);
         res.end();
-    })
-})
+    });
+});
 
 
-module.exports = router
+module.exports = router;
