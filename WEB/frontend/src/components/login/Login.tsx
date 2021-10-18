3acb7ddcd5dfe2ab81eb9826bba04d741f762124
Moon Kyung Song <moonsong98@postech.ac.kr>
2021-10-18 10:47:01 +0000

    Connect Login Component with Server

--- WEB/frontend/src/components/login/Login.tsx
+++ WEB/frontend/src/components/login/Login.tsx
@@ -1,45 +1,29 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import LoginPresenter from "./LoginPresenter";
-import {
-  useAuthState,
-  useAuthDispatch,
-} from "../../utils/contexts/AuthContext";
+import { useAuthDispatch } from "../../utils/contexts/AuthContext";
+import api from "../../utils/api";
 
 export default function Login() {
+  const [isError, setIsError] = useState(false);
   const authDispatch = useAuthDispatch();
   const handleSubmit = (loginInput: { username: string; password: string }) => {
-    authDispatch({
-      type: "LOGIN",
-      username: loginInput.username,
-      accessToken: "",
-      isAuth: true,
-    });
-    window.localStorage.setItem("isAuth", JSON.stringify(true));
-    /*
-    axios({
-      url: "https://osamhack2021-cloud-web-blo-my-byzantium-v5jxjj4w2qx5-8000.githubpreview.dev/account/login/",
-      method: "post",
-      headers: {
-        "Content-Type": "text/plain",
-      },
-      data: {
-        username: "20-12345677",
-        password: "test123",
-      },
-      withCredentials: true,
-    }).then((res) => {
-      console.log(res);
-      if (res.status === 200) {
+    api
+      .post("/account/login/", {
+        ...loginInput,
+      })
+      .then((res) => {
+        setIsError(false);
         authDispatch({
           type: "LOGIN",
           username: loginInput.username,
           accessToken: "",
           isAuth: true,
         });
-      }
-    });
-    */
+        window.localStorage.setItem("isAuth", JSON.stringify(true));
+      })
+      .catch(() => {
+        setIsError(true);
+      });
   };
-  return <LoginPresenter handleSubmit={handleSubmit} />;
+  return <LoginPresenter isError={isError} handleSubmit={handleSubmit} />;
 }
